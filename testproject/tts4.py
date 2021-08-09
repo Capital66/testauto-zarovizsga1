32f5341dec0d957204aef730803e2390c31fe4a8
Knap Tamás <knaptamas@gmail.com>
2021-08-09 19:38:21 +0200

    2 Feladat megoldas

--- testproject/tts4.py
+++ testproject/tts4.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
